bb4eaa4e671a17954df0bf5794bde7f639daed2b
YepesF <yepilef@gmail.com>
2022-10-30 17:18:06 -0500

    Add time stands to DB

--- api/src/models/Convertion.js
+++ api/src/models/Convertion.js
@@ -2,27 +2,21 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define(
-    "convertion",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV1,
-        allowNull: false,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-      },
-      cop: {
-        type: DataTypes.STRING,
-      },
-      usd: {
-        type: DataTypes.STRING,
-      },
+  sequelize.define("convertion", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      allowNull: false,
+      primaryKey: true,
     },
-    {
-      timestamps: false,
-    }
-  );
+    name: {
+      type: DataTypes.STRING,
+    },
+    cop: {
+      type: DataTypes.STRING,
+    },
+    usd: {
+      type: DataTypes.STRING,
+    },
+  });
 };
