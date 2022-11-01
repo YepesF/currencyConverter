b7095729eab2325ebb765ecc7e6404b9fd47e5f9
YepesF <yepilef@gmail.com>
2022-11-01 05:08:41 -0500

    Create conditionals for flow chatbot

--- client/src/ChatBot/MessageParser.js
+++ client/src/ChatBot/MessageParser.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 const MessageParser = ({ children, actions }) => {
-  const parse = (message) => {
-    if (message > 0) {
-      actions.handleConvert(message);
-    } else {
+  const [data, setData] = useState({
+    name: "",
+    amount: "",
+  });
+  const parse = async (message) => {
+    const { name, amount } = data;
+
+    if (!name && !amount) {
       actions.handleHello(message);
+      setData((prev) => {
+        return { ...prev, name: message };
+      });
     }
+
+    if (name && !amount) {
+      if (message > 0) {
+        actions.handleConvert(message);
+        setData((prev) => {
+          return { ...prev, amount: message };
+        });
+      }
+
+      if (!Number(message) || message <= 0) {
+        actions.handleError();
+      }
+    }
+
+    // if (message > 0) {
+    //   actions.handleConvert(message);
+    // } else {
+    //   actions.handleHello(message);
+    // }
   };
   return (
     <div>
