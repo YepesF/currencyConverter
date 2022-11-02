7cb1a2bf54ce1642a89b5dffcb5954bddad23a18
YepesF <yepilef@gmail.com>
2022-11-01 19:43:14 -0500

    Register widget in chatbot config

--- client/src/ChatBot/config.js
+++ client/src/ChatBot/config.js
@@ -1,11 +1,21 @@
 import { createChatBotMessage } from "react-chatbot-kit";
+import Options from "../Components/Widgets/Options/Options";
 
 const config = {
   botName: "CurrencyBot",
   initialMessages: [
     createChatBotMessage(
-      `Welcome to de Converter Currecy ChatBot, What's your name?`
+      `Welcome to de Converter Currecy ChatBot, What's your name?`,
+      {
+        widget: "options",
+      }
     ),
+  ],
+  widgets: [
+    {
+      widgetName: "options",
+      widgetFunc: (props) => <Options {...props} />,
+    },
   ],
   customStyles: {
     botMessageBox: {
