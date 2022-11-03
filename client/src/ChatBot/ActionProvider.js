f87e73fc8c82a3773acee37735ea1d9127702456
YepesF <yepilef@gmail.com>
2022-11-03 16:10:32 -0500

    Modify actions for state

--- client/src/ChatBot/ActionProvider.js
+++ client/src/ChatBot/ActionProvider.js
@@ -1,44 +1,84 @@
 import React from "react";
 import axios from "axios";
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-  let data = { name: "", amount: "" };
+  const { name } = children.props.children.props.state;
 
   const handleHello = (name) => {
-    data = { ...data, name };
+    setState((prev) => ({
+      ...prev,
+      name,
+    }));
     const botMessage = createChatBotMessage(
       `Hello ${name}. What's the amount for convert  to dollars?`
     );
-    setState((prev) => ({ ...prev, messages: [...prev.messages, botMessage] }));
+    addMessageToState(botMessage);
   };
 
   const handleError = () => {
     const botMessage = createChatBotMessage(
       `The amount is invalid, please enter a number greater a 1`
     );
-    setState((prev) => ({ ...prev, messages: [...prev.messages, botMessage] }));
+    addMessageToState(botMessage);
   };
 
-  const handleConvert = (amount) => {
-    data = { ...data, amount };
+  const handleConvert = async (amount) => {
+    let res = false;
 
-    axios.post("http://localhost:3001/convert", data).then((response) => {
-      const botMessage = createChatBotMessage(
-        `${amount}COP are ${response.data.result}USD. Do you want convert other currency?.`
-      );
-      setState((prev) => ({
-        ...prev,
-        messages: [...prev.messages, botMessage],
-      }));
-    });
+    setState((prev) => ({
+      ...prev,
+      amount,
+    }));
+
+    await axios
+      .post("http://localhost:3001/convert", { name, amount })
+      .then((response) => {
+        res = true;
+        const botMessage = createChatBotMessage(
+          `${amount}COP are ${response.data.result}USD. Do you want convert other currency?.`,
+          {
+            widget: "options",
+          }
+        );
+        addMessageToState(botMessage);
+      })
+      .catch(() => {
+        const botMessage = createChatBotMessage(
+          `Excuse me ${name}, we'll a problem in the application, please retry again. What's the amount for convert  to dollars?`
+        );
+        addMessageToState(botMessage);
+      });
+
+    return res;
   };
 
   const handleMain = () => {
-    // console.log("Soy Felipe");
-    data = { name: "", amount: "" };
     const botMessage = createChatBotMessage(
       `Welcome to de Converter Currecy ChatBot, What's your name?`
     );
+    setState((prev) => ({
+      ...prev,
+      messages: [botMessage],
+      name: "",
+      amount: "",
+      converted: false,
+    }));
+  };
+
+  const handleConfirmation = () => {
+    const botMessage = createChatBotMessage(
+      `Excuse me I don't understand. Do you want convert other currency?.`,
+      {
+        widget: "options",
+      }
+    );
     setState((prev) => ({ ...prev, messages: [botMessage] }));
+  };
+
+  const addMessageToState = (message) => {
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, message],
+    }));
   };
 
   return (
@@ -50,6 +90,7 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
             handleConvert,
             handleError,
             handleMain,
+            handleConfirmation,
           },
         });
       })}
